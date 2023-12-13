8b1bf461fb802ca92129f17cbdebf25964003dc2
thaigoklemga <thiagoklemg@gmail.com>
2023-12-13 20:14:18 -0300

    Fix gitignore and env

--- backend/src/tests/integration/contacts.test.ts
+++ backend/src/tests/integration/contacts.test.ts
@@ -2,25 +2,27 @@ import request from "supertest";
 import { app } from "../../server";
 import contacts from "../../data";
 
-test("GET /contacts", async () => {
-  const response = await request(app).get("/contacts");
+describe("Testing routes", () => {
+  test("GET /contacts", async () => {
+    const response = await request(app).get("/contacts");
 
-  expect(response.status).toBe(200);
-  expect(response.body.success).toBe(true);
-  expect(response.body.message).toBe("Contacts retrieved successfully");
-  expect(response.body.data).toEqual(contacts);
-});
+    expect(response.status).toBe(200);
+    expect(response.body.success).toBe(true);
+    expect(response.body.message).toBe("Contacts retrieved successfully");
+    expect(response.body.data).toEqual(contacts);
+  });
 
-test("POST /contacts/:index", async () => {
-  const updatedContact = {
-    firstname: "John",
-    lastname: "Doe",
-    phone: "1233211234",
-  };
+  test("POST /contacts/:index", async () => {
+    const updatedContact = {
+      firstname: "John",
+      lastname: "Doe",
+      phone: "1233211234",
+    };
 
-  const response = await request(app).post(`/contacts`).send(updatedContact);
+    const response = await request(app).post(`/contacts`).send(updatedContact);
 
-  expect(response.status).toBe(201);
-  expect(response.body.success).toBe(true);
-  expect(response.body.message).toBe("Contact added successfully");
+    expect(response.status).toBe(201);
+    expect(response.body.success).toBe(true);
+    expect(response.body.message).toBe("Contact added successfully");
+  });
 });
